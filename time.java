06d5275fb6ec4b5beac56bce96dab02f45f06c82
andreabeatrice <andreaxblignaut@gmail.com>
2022-03-09 10:13:08 +0200

    Update time.java
    Reformat

--- time.java
+++ time.java
@@ -5,33 +5,66 @@ import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+
+/**
+ * time.java<p>
+ *
+ * <p>
+ * Usage:  This library of java functions, which Pat L. Durante has encapsulated inside
+ *         a class called cgi_lib as class (static) member functions,
+ *         attempts to duplicate the standard PERL CGI library (cgi-lib.pl).
+ *
+ *
+ *         (You are only allowed to use code that you found on the Web and elsewhere if
+ *         the licence conditions of that code allows you to use it. If you use such code, it
+ *         should be clearly documented that you used the code (as well as where you got it from)).<p>
+ * 
+ *         Methods provided by Pat L. Durante:
+ *            {@link #Header() Header}
+ *            {@link #HtmlTop(String Title) Title}
+ *            {@link #urlDecode(String in) urlDecode}
+ *            {@link #ReadParse(InputStream inStream) ReadParse}
+ *            {@link #Variables(Hashtable form_data) Variables}
+ *            {@link #HtmlBot() HtmlBot}
+ *            {@link #MethGet() MethGet}
+ *            {@link #MethPost() MethPost}
+ *            {@link #MyBaseURL() MyBaseURL}
+ *            {@link #MyFullURL() MyFullURL}
+ *            {@link #Environment() Environment}
+ *         
+ *         @see https://www.infoworld.com/article/2076863/write-cgi-programs-in-java.html#:~:text=A%20CGI%20program%20can%20be,interactive%20applications%20to%20Web%20sites
+ *
+ * @version 1.0
+ * @author Pat L. Durante
+ *
+ */
+
 @SuppressWarnings("unchecked")
 class time {
-
-  /**
-   *
-   * Generate a standard HTTP HTML header.
-   *
-   * @return A String containing the standard HTTP HTML header.
-   *
-   */
-	public static String Header()
-	{
-		return "Content-type: text/html\n\n";
+ 
+ /**
+ *
+ * Generate a standard HTTP HTML header.
+ *
+ * @return A String containing the standard HTTP HTML header.
+ *
+ */
+	public static String Header(){
+    return "Content-type: text/html\n\n";
 	}
 
-  /**
-   *
-   * Generate some vanilla HTML that you usually
-   * want to include at the top of any HTML page you generate.
-   *
-   * @param Title The title you want to put on the page.
-   *
-   * @return A String containing the top portion of an HTML file.
-   *
-   */
-	public static String HtmlTop(String Title)
-	{
+ /**
+ *
+ * Generate some vanilla HTML that you usually
+ * want to include at the top of any HTML page you generate.
+ *
+ * @param Title The title you want to put on the page.
+ *
+ * @return A String containing the top portion of an HTML file.
+ *
+ */
+	public static String HtmlTop(String Title){
+
 		String Top = new String();
 		Top = "<!DOCTYPE HTML>\n";
 		Top += "<html>\n";
@@ -66,8 +99,8 @@ class time {
    *
    */
 
-  public static String urlDecode(String in)
-  {
+  public static String urlDecode(String in){
+
       StringBuffer out = new StringBuffer(in.length());
       int i = 0;
       int j = 0;
@@ -119,8 +152,8 @@ class time {
    * in which the keys represent the names of the input fields.
    *
    */
-  public static Hashtable ReadParse(InputStream inStream)
-  {
+  public static Hashtable ReadParse(InputStream inStream){
+
       Hashtable form_data = new Hashtable();
 
       String inBuffer = "";
@@ -190,8 +223,7 @@ class time {
    * form variables and the associated values.
    *
    */
-  public static String Variables(Hashtable form_data)
-  {
+  public static String Variables(Hashtable form_data) {
 
       String returnString;
 
@@ -221,8 +253,8 @@ class time {
    * @return A String containing the bottom portion of an HTML file.
    *
    */
-  public static String HtmlBot()
-  {
+  public static String HtmlBot(){
+
       return "</body>\n</html>\n";
   }
 
@@ -234,8 +266,7 @@ class time {
    * @return true, if the REQUEST_METHOD was GET.  false, otherwise.
    *
    */
-  public static boolean MethGet()
-  {
+  public static boolean MethGet() {
      String RequestMethod = System.getProperty("cgi.request_method");
      boolean returnVal = false;
 
@@ -258,8 +289,7 @@ class time {
    * @return true, if the REQUEST_METHOD was POST.  false, otherwise.
    *
    */
-  public static boolean MethPost()
-  {
+  public static boolean MethPost() {
      String RequestMethod = System.getProperty("cgi.request_method");
      boolean returnVal = false;
 
@@ -282,8 +312,7 @@ class time {
    * @return The Base URL of this script as a String.
    *
    */
-  public static String MyBaseURL()
-  {
+  public static String MyBaseURL() {
       String returnString = new String();
       returnString = "http://" +
                      System.getProperty("cgi.server_name");
@@ -302,8 +331,7 @@ class time {
    * @return The Full URL of this script as a String.
    *
    */
-  public static String MyFullURL()
-  {
+  public static String MyFullURL() {
       String returnString;
       returnString = MyBaseURL();
       returnString += System.getProperty("cgi.path_info");
@@ -322,8 +350,7 @@ class time {
    * variables and the associated values.
    *
    */
-  public static String Environment()
-  {
+  public static String Environment(){
       String returnString;
 
       returnString = "<dl compact>\n";
@@ -357,84 +384,86 @@ class time {
       return returnString;
   }
 
-	 static void getTime() throws FileNotFoundException{
+  /**
+   *
+   * Get and print the time at moment of execution
+   *
+   *
+   */
+  static void getTime() throws FileNotFoundException{
+
 		Scanner input = new Scanner(new File("GMT.txt"));
 		int current = input.nextInt();
 
-		 Date date = Calendar.getInstance().getTime();
-                DateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
-                String strDate = dateFormat.format(date);
+    Date date = Calendar.getInstance().getTime();
+    DateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
+    String strDate = dateFormat.format(date);
 		TimeZone tz = TimeZone.getDefault();
-               // System.out.println("Converted String: " + strDate);
 
 		if (current == 2)
-                	System.out.println(dateFormat.format(date) + " " + tz.getDisplayName());
+      System.out.println(dateFormat.format(date) + " " + tz.getDisplayName());
 		else {
-			 SimpleDateFormat sdfGhana = new SimpleDateFormat("hh:mm:ss");
-        		TimeZone tzInGhana = TimeZone.getTimeZone("Africa/Accra");
-        		sdfGhana.setTimeZone(tzInGhana);
+      SimpleDateFormat sdfGhana = new SimpleDateFormat("hh:mm:ss");
+      TimeZone tzInGhana = TimeZone.getTimeZone("Africa/Accra");
+      sdfGhana.setTimeZone(tzInGhana);
 
-        		String sDateInGH = sdfGhana.format(date); // Convert to String first
+      String sDateInGH = sdfGhana.format(date); // Convert to String first
 			System.out.println(sDateInGH + " Ghanaian Standard Time");
 		}
 	}
 
-	public static void main( String args[] ) throws FileNotFoundException
-	{
-		System.out.println(Header());
-		System.out.println(HtmlTop("Hello World"));
+	public static void main( String args[] ) throws FileNotFoundException {
 
-		//
+    //
 		//  Parse the form data into a Hashtable.
 		//
-		Hashtable form_data = ReadParse(System.in);
+      Hashtable form_data = ReadParse(System.in);
 
-		//
-		//  Print out each of the name/value pairs
-		//  from sent from the browser.
-		//
-		//System.out.println(Variables(form_data));
-		//System.out.println("<hr>");
+    //
+    //Get the action the user chose
+    //
+		  String decision = (String)form_data.get("action");
 
-		String decision = (String)form_data.get("action");
-		if (decision.equals("display")){
-			getTime();
-		}
+    //
+    //Add basic HTML Layout
+    //
+      System.out.println(Header());
+      System.out.println(HtmlTop(decision));
+		
+    if (decision.equals("display")){
+      getTime();
+    }
 		else {
-			int change = 1;
-			if (decision.equals("ghana")){
-				change = 0;
-			}
-			if (decision.equals("za")){
-				change = 2;
-			}
 			try {
-      				FileWriter myWriter = new FileWriter("GMT.txt");
-     				if (decision.equals("ghana"))
-					myWriter.write("0");
+        FileWriter myWriter = new FileWriter("GMT.txt");
+        
+        if (decision.equals("ghana"))
+          myWriter.write("0");
 
 				if (decision.equals("za"))
 					myWriter.write("2");
      				 
 				myWriter.close();
-    			} catch (IOException e) {
-      				System.out.println("An error occurred.");
-      				e.printStackTrace();
-    			}
+    	} catch (IOException e) {
+      	System.out.println("An error occurred.");
+      	e.printStackTrace();
+    	}
+
 			getTime();
 		}
 
 		System.out.print("<form method='POST' action='/cgi-bin/ghanaian.cgi'>\n");
 		System.out.print("<input type='hidden' value='ghana' name='action'>\n");
-                System.out.print(" <input type='submit'  name='act' value='Switch to Ghanaian Time'/>\n");
-           	System.out.print("</form>\n");
+    System.out.print(" <input type='submit'  name='act' value='Switch to Ghanaian Time'/>\n");
+    System.out.print("</form>\n");
 
-	        System.out.print("<br/>\n");
+    System.out.print("<br/>\n");
 
 		System.out.print("<form method='POST' action='/cgi-bin/southafrican.cgi'>\n");
-                System.out.print("<input type='hidden' value='za' name='action'>\n");
-                System.out.print(" <input type='submit'  name='act' value='Switch to South African Time'/>\n");
-                System.out.print("</form>\n");
+    System.out.print("<input type='hidden' value='za' name='action'>\n");
+    System.out.print(" <input type='submit'  name='act' value='Switch to South African Time'/>\n");
+    System.out.print("</form>\n");
+
 		System.out.println(HtmlBot());
 	}
 }
