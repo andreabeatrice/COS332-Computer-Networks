358d0084b73dcfbe2485d54aa5718be0f2558ee1
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-03-27 16:42:42 +0200

    INSERT funct. added

--- Prac2/Client.java
+++ Prac2/Client.java
@@ -5,20 +5,27 @@ public class Client{
 
 
 	private Socket			socket = null;
-	private ServerSocket	server = null;
+	private DataInputStream	input = null;
 	private DataOutputStream 	out = null;
 
 
 	//constructor with port #
-	public Client (string address, int port){
+	public Client (String address, int port){
 		try{
 
 			socket = new Socket(address, port);
 			System.out.println("Connected");
+			System.out.println("******Server Commands***** \nDISPLAY - Print database");
+			
+			System.out.println("SEARCH (<friend name> || <friend cell number>) - Searches the database for a record with the given parameter");
+			System.out.println("INSERT (<friend name>, <friend cell number>) - Adds a record to the database");
+			System.out.println("DELETE (<friend name> || <friend cell number>) - Deletes a record from the database using the provided parameter to find the record");
+			System.out.println("UPDATE - Changes a record in the database. Enter ? to see options");
+			System.out.println("UPDATE (<original number>, <new number>) - Changes a record in the database");
 
 			input = new DataInputStream(System.in);
 
-			out = new DataOutputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
 		}
 		catch (UnknownHostException u ){
 			System.out.println(u);
@@ -41,6 +48,6 @@ public class Client{
 	}
 
 	public static void main(String [] args){
-		Client client = new Client(10.0.0.2, 5000);
+		Client client = new Client("10.0.0.2", 5000);
 	}
 }
--- Prac2/Server.java
+++ Prac2/Server.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class Server{
 
@@ -25,10 +26,16 @@ public class Server{
 
 			String line = "";
 
-			while (!line.equals("Over")) {
+			while (!line.equals("QUIT")) {
 				try {
 					line = in.readUTF();
-					System.out.println(line);
+					//System.out.println(line);
+
+					if (line.contains("INSERT")) {
+						Scanner input = new Scanner(line).useDelimeter("(");
+						String entry = input.next();
+						System.out.println(insert(entry));
+					}
 				}
 				catch (IOException i){
 					System.out.println(i);
@@ -45,6 +52,27 @@ public class Server{
 			System.out.println(i);
 		}
 	}
+
+
+	public String insert(String en){
+		String result = "";
+
+		try {
+      		FileWriter myWriter = new FileWriter("database.txt");
+
+      		String entry = en + "\n";
+
+     		myWriter.write(entry);
+      		myWriter.close();
+      		result = "Successfully wrote to the file.";
+
+	    } catch (IOException e) {
+	      result = System.out.println("An error occurred.");
+	    }
+
+		return result;
+	}
+
 
 	public static void main(String [] args){
 		Server server = new Server(5000);
