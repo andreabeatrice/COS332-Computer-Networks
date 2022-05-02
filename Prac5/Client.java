6eb3c7e5e03dd4027433e1c54be414471431f5c1
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-05-02 11:55:33 +0200

    Prac5 created

---
+++ Prac5/Client.java
@@ -0,0 +1,57 @@
+import java.net.*;
+import java.io.*;
+
+public class Client{
+
+
+	private Socket			socket = null;
+	private DataInputStream	input = null;
+	private DataOutputStream 	out = null;
+
+
+	//constructor with port #
+	public Client (String address, int port){
+		try{
+
+			socket = new Socket(address, port);
+			System.out.println("Connected");
+			System.out.println("******Server Commands***** \n\nDISPLAY - Print database");
+			
+			System.out.println("SEARCH (<friend name>) - Searches the database for a record with the given parameter");
+			System.out.println("INSERT (<friend name>, <friend cell number>) - Adds a record to the database");
+			System.out.println("DELETE (<friend name> || <friend cell number>) - Deletes a record from the database using the provided parameter to find the record");
+			System.out.println("QUIT - Ends interaction with the database.\n");
+			System.out.println("UPDATE - Changes a record in the database.\n");
+			System.out.println("\tUPDATE -n (<original name>, <new name>)\n");
+			System.out.println("\tUPDATE -t (<original name>, <new number>)\n");
+
+			input = new DataInputStream(System.in);
+
+			out = new DataOutputStream(socket.getOutputStream());
+		}
+		catch (UnknownHostException u ){
+			System.out.println(u);
+		}
+		catch (IOException i){
+			System.out.println(i);
+		}
+
+		String line = "";
+
+		while (!line.equals("QUIT")) {
+
+				try {
+					line = input.readLine();
+					out.writeUTF(line);
+
+				}
+				catch (IOException i){
+					System.out.println(i);
+				}
+			}
+	}
+
+	public static void main(String [] args){
+		Client client = new Client("10.0.0.2", 5000);
+	}
+}
---
+++ Prac5/Server.java
@@ -0,0 +1,331 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class Server{
+
+
+	private Socket			socket = null;
+	private ServerSocket	server = null;
+	private DataInputStream 	in = null;
+	private PrintWriter output = null;
+
+
+	//constructor with port #
+	public Server (int port){
+		try{
+
+			server = new ServerSocket(port);
+			System.out.println("Server started");
+
+			System.out.println("Waiting for client...");
+
+			socket = server.accept();
+			System.out.println("Client accepted");
+
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+			output = new PrintWriter(socket.getOutputStream(), true);
+			
+	BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			output.println("Connected" );
+			output.write(13);
+			output.write(10);
+			output.println("******Server Commands*****");
+			output.write(13);
+			output.write(10);
+			output.println("DISPLAY - Print database");
+			
+			output.write(13);
+			output.write(10);
+			output.println("SEARCH (<friend name>) - Searches the database for a record with the given parameter");
+			output.write(13);
+			output.write(10);
+			output.println("INSERT (<friend name>, <friend cell number>) - Adds a record to the database");
+			output.write(13);
+			output.write(10);
+			output.println("DELETE (<friend name> || <friend cell number>) - Deletes a record from the database using the provided parameter");
+			output.write(13);
+			output.write(10);
+			output.println("QUIT - Ends interaction with the database.");
+			output.write(13);
+			output.write(10);
+			
+			output.println("UPDATE - Changes a record in the database.");
+			output.write(13);
+			output.write(10);
+			output.write(9);
+			output.println("UPDATE -n (<original name>, <new name>)");
+			output.write(13);
+			output.write(10);
+			output.write(9);
+			output.println("UPDATE -t (<original name>, <new number>)");
+			output.write(13);
+			output.write(10);
+			
+			output.write(27);
+			output.println("[0G ");
+			
+			String line = "";
+
+			while (!line.equals("QUIT")) {
+				try {
+					line = reader.readLine();
+					System.out.println(line);
+
+					if (line.contains("INSERT")) {
+						output.write(13);
+						output.write(10);
+						insert(line);
+					}
+					if (line.contains("DISPLAY")) {
+					//System.out.println(display());
+						output.write(13);
+						output.write(10);
+						display();
+					}
+					if (line.contains("SEARCH")) {
+						output.write(13);
+						output.write(10);
+						search(line);
+					}
+					if (line.contains("DELETE")) {
+						output.write(13);
+						output.write(10);
+						delete(line);
+					}
+					if (line.contains("UPDATE")) {
+						output.write(13);
+						output.write(10);
+						update(line);
+					}
+					if (line.contains("CLEAR")){
+						output.println(clearConsole());
+						output.flush();
+					}
+				}
+				catch (IOException i){
+					output.println(i);
+				}
+			}
+
+			System.out.println("Close connection");
+
+			socket.close();
+			in.close();
+
+		}
+		catch (IOException i){
+			System.out.println(i);
+		}
+	}
+
+
+	public void insert(String en){
+		String result = "";
+
+		try {
+      		FileWriter myWriter = new FileWriter("database.txt", true);
+
+      		String entry = en.substring(8, en.length()-1) + "\n";
+
+     		myWriter.write(entry);
+      		myWriter.close();
+      		//result = ;
+      		
+      		output.write(13);
+			output.write(10);
+			output.println("Successfully wrote to the file.");
+
+	    } catch (IOException e) {
+	      //result = "An error occurred.";
+	    }
+
+		output.write(13);
+		output.write(10);
+		output.write(27);
+		output.println("[0G ");
+	}
+
+	public void display(){
+		output.println("FRIEND, TELEPHONE NUMBER");
+
+
+		try {
+      		Scanner input = new Scanner(new File("database.txt"));
+      		input.useDelimiter("\n");
+      		while (input.hasNext()){
+				output.write(13);
+				output.write(10);
+				output.println(input.next());
+      			//result += nl + input.next();
+      		}
+
+      		input.close();
+
+	    } catch (IOException e) {
+	      //result = "An error occurred.";
+	    }
+
+		//return result;
+		output.write(13);
+		output.write(10);
+		output.write(27);
+		output.println("[0G ");
+	}
+
+
+	public void search(String en){
+		output.println("SEARCH RESULTS: ");
+
+		String searchterm = en.substring(8, en.length()-1);
+
+
+		try {
+      		Scanner input = new Scanner(new File("database.txt"));
+      		input.useDelimiter("\n");
+      		while (input.hasNextLine()){
+      			String line = input.nextLine();
+      			if (line.contains(searchterm)){
+      				output.write(13);
+					output.write(10);
+					output.println(line);
+      			}
+      			
+      		}
+
+      		input.close();
+
+	    } catch (IOException e) {
+	      //result = "An error occurred.";
+	    }
+
+
+	    output.write(13);
+		output.write(10);
+		output.write(27);
+		output.println("[0G ");
+	}
+
+	public void delete(String en){
+
+		String searchterm = en.substring(8, en.length()-1);
+		String result = "";
+		boolean del = false;
+		try {
+      		Scanner input = new Scanner(new File("database.txt"));
+      		input.useDelimiter("\n");
+      		while (input.hasNextLine()){
+      			String line = input.nextLine();
+      			if (!line.contains(searchterm)){
+      				result += line +"\n";
+      			}
+      			else {
+      				del = true;
+      			}
+      			
+      		}
+
+      		input.close();
+
+      		FileWriter myWriter = new FileWriter("database.txt");
+
+     		myWriter.write(result);
+      		myWriter.close();
+
+      		if (del) {
+      			output.write(13);
+				output.write(10);
+      			output.println("Record Successfully Deleted");
+      		}
+      		else {
+      			output.write(13);
+				output.write(10);
+      			output.println("Nothing to delete.");
+      		}
+
+      		
+      		output.write(13);
+			output.write(10);
+			output.write(27);
+			output.println("[0G ");
+
+	    } catch (IOException e) {
+
+	      //return "An error occurred.";
+	    }
+
+	}
+	
+	public String clearConsole() {   
+		return "\033[H\033[2J";
+	}
+
+
+	public void update(String en){
+
+		String searchterm = en.substring(11, en.length()-1);
+		String updatetype = en.substring(8,9);
+		boolean change = false;
+
+
+		Scanner enTerm = new Scanner(en).useDelimiter(",");
+		String entry = enTerm.next(); //<original name>
+		String newVal = enTerm.next();
+
+		newVal = newVal.substring(1, newVal.length()-1);
+		entry = entry.substring(11, entry.length());
+
+		String result = "";
+		
+		try {
+      		Scanner input = new Scanner(new File("database.txt"));
+      		input.useDelimiter("\n");
+      		while (input.hasNextLine()){
+      			String line = input.nextLine();
+      			if (!line.contains(entry)){
+      				result += line +"\n";
+      			}
+      			else {
+      				change = true;
+      				Scanner lin = new Scanner(line).useDelimiter(",");
+      				if (updatetype.equals("n")) {
+      					lin.next();
+      					result += newVal + "," + lin.next()+"\n";
+      				}
+      				else {
+      					result += lin.next() + "," + newVal+"\n";
+      				}
+      			}
+      			
+      		}
+
+      		input.close();
+
+      		FileWriter myWriter = new FileWriter("database.txt");
+
+     		myWriter.write(result);
+      		myWriter.close();
+
+      		if (change) {
+      			output.write(13);
+				output.write(10);
+      			output.println("Record Successfully Changed");
+      		}
+      		else {
+      			output.write(13);
+				output.write(10);
+      			output.println("Nothing to change.");
+      		}
+
+	    } catch (IOException e) {
+	      //return "An error occurred.";
+	    }
+
+	}
+
+	public static void main(String [] args){
+		Server server = new Server(5000);
+	}
+}
