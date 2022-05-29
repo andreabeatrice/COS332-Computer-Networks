04f3a3a6063cd5a5e1add50d6d7409883d505c7a
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-05-29 18:19:26 +0200

    plain java files created

---
+++ Prac9/plain java/ServerListenerThread.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ServerListenerThread extends Thread {
+
+    private Socket clientSocket, clientActor;
+    private PrintWriter output;
+    private ServerSocket serverActor;
+
+    private BufferedReader br;
+
+    public ServerListenerThread(Socket clientSocket, Socket clientActor, ServerSocket serverActor) throws IOException {
+        this.clientSocket = clientSocket;
+        this.clientActor = clientActor;
+        this.serverActor = serverActor;
+
+        output = new PrintWriter(clientSocket.getOutputStream(), false,StandardCharsets.ISO_8859_1); //,
+        br = new BufferedReader(new InputStreamReader(clientActor.getInputStream(),StandardCharsets.ISO_8859_1));
+    }
+
+    @Override
+    public void run(){
+        System.out.println("Server Listener started");
+        try {
+            while (serverActor.isBound() && !serverActor.isClosed()) {
+                int red = -1;
+                byte[] buffer = new byte[5*1024]; // a read buffer of 5KiB
+                byte[] redData;
+                StringBuilder clientData = new StringBuilder();
+                String redDataText;
+                red = br.read();
+                redData = new byte[red];
+                System.arraycopy(buffer, 0, redData, 0, red);
+                //output.write(redData);
+
+                redDataText = new String(redData); // assumption that client sends data UTF-8 encoded
+                System.out.println("SLT-" + red + " ");
+                clientData.append(redDataText);
+                output.write(red);
+                output.flush();
+
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Problem with setting socket " + e);
+        } finally {
+            if (serverActor!=null){
+                try {
+                    serverActor.close();
+                } catch (IOException e) {}
+            }
+        }
+    }
+}
---
+++ Prac9/plain java/telnetproxy.java
@@ -0,0 +1,74 @@
+import ClientListenerThread;
+import ServerListenerThread;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+
+public class telnetproxy {
+
+    private Socket socket;
+    private Socket clientActor;
+    private Writer pw;
+    private BufferedReader br, bf;
+
+    private ServerSocket serverActor;
+
+    private DataInputStream in;
+    private DataOutputStream out;
+    private PrintWriter output;
+
+    public telnetproxy (String address, int port){
+        try {
+            //Server Actor
+            serverActor = new ServerSocket(55555);
+            System.out.println("Proxy started. Waiting for client...");
+
+            socket = serverActor.accept();
+            System.out.println("Client accepted");
+
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+            out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+            output = new PrintWriter(socket.getOutputStream(), true); //, StandardCharsets.ISO_8859_1
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.ISO_8859_1));
+
+            //Client Actor
+            clientActor = new Socket("10.0.0.2", 23);
+
+            pw = new PrintWriter(clientActor.getOutputStream(), true);
+
+            br = new BufferedReader(new InputStreamReader(clientActor.getInputStream(), StandardCharsets.ISO_8859_1)); //
+
+            ServerListenerThread slt = new ServerListenerThread(socket, clientActor, serverActor);
+            slt.start();
+
+            ClientListenerThread clt = new ClientListenerThread(socket, clientActor, serverActor);
+            clt.start();
+
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void main(String [] args) throws IOException {
+        DataInputStream input = new DataInputStream(System.in);
+
+        String line = input.readLine();
+
+
+        if (line.contains("telnet") || line.contains("o ")){
+            telnetproxy client = new telnetproxy("10.0.0.2", 23);
+
+        }
+
+    }
+}
