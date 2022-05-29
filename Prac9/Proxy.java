749003663917785aa819b347dc312e902269ce82
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-05-29 10:29:13 +0200

    Create Proxy.java

---
+++ Prac9/Proxy.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class Proxy{
+
+	private ServerSocket serverActor = null;
+	private Socket clientActor = null;
+	private Socket clientAcceptor = null;
+
+
+	private PrintWriter proxyOutputWriter = null;  //output
+
+    private BufferedReader wClientInputReader = null; //input //bf
+
+    private PrintWriter writeToServer = null; //pw
+
+    private BufferedReader tServerResponse = null; //br
+
+	public Proxy(int port){
+
+		try{
+
+			//Connect to windows client
+			serverActor = new ServerSocket(port);
+			System.out.println("Proxy started. Waiting for connection...");
+
+
+			clientAcceptor = serverActor.accept();
+			System.out.println("Client connected.");
+
+			proxyOutputWriter = new PrintWriter(clientAcceptor.getOutputStream(), true);
+			
+			BufferedReader wClientInputReader = new BufferedReader(new InputStreamReader(clientAcceptor.getInputStream()));
+
+
+			//Connect to telnet server
+
+			clientActor = new Socket("10.0.0.2", 23);
+
+			writeToServer = new PrintWriter(clientActor.getOutputStream(),true);
+
+			tServerResponse = new BufferedReader(new InputStreamReader(clientActor.getInputStream()));
+
+			String msg = tServerResponse.readLine();
+
+			//System.out.println(msg);
+			proxyOutputWriter.println(msg);
+
+
+		}
+		catch (IOException i){
+			System.out.println(i);
+		}
+
+	}
+
+
+	public static void main(String [] args){
+
+
+
+		//Server server = new Server(PORT);
+	}
+}
